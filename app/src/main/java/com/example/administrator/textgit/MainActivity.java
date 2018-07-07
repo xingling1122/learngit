dec85df195eb37860260c75f64b342e1c7ec73d0
xingling <1579485289@qq.com>
2018-07-07 17:08:58 +0800

    v3

--- app/src/main/java/com/example/administrator/textgit/MainActivity.java
+++ app/src/main/java/com/example/administrator/textgit/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);//v1//v2
+        setContentView(R.layout.activity_main);//v1//v2//v3
     }
 }
